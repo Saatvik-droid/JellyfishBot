3ed94fce8e9336f32c3a64657be1e7a64506677a
SaatvikRamani <saatvikramani777@gmail.com>
2020-07-01 19:17:10 +0530

    added database to store scores

---
+++ _models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, Column, Integer, BIGINT
+from sqlalchemy.ext.declarative import declarative_base
+
+from config import DATABASE_URI
+
+Base = declarative_base()
+
+class Scores(Base):
+    __tablename__ = "scores"
+
+    id = Column("id", Integer, primary_key=True)
+    user_id = Column("user_id", BIGINT, unique=True, nullable=False)
+    score = Column("score", Integer, default=0)
+
+engine = create_engine(DATABASE_URI, echo=True)
+Base.metadata.create_all(bind=engine)
+
--- cogs/catcher.py
+++ cogs/catcher.py
@@ -1,7 +1,19 @@
 import discord
 from discord.ext import commands
 
-from config import CATCH_PENDING_ID
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, BIGINT
+
+from config import CATCH_PENDING_ID, DATABASE_URI
+
+engine = create_engine(DATABASE_URI)
+meta = MetaData()
+
+scores = Table(
+                "scores", meta,
+                Column("id", Integer, primary_key=True),
+                Column("user_id", BIGINT, unique=True, nullable=False),
+                Column("score", Integer, default=0),
+)
 
 class Catcher(commands.Cog):
 
@@ -14,6 +26,17 @@ class Catcher(commands.Cog):
         for user in message.mentions:
             if user.id == self.bot.user.id and message.channel.id in CATCH_PENDING_ID:
                 await message.channel.send(f"Congratulations <@{message.author.id}> caught a jelly")
+                conn = engine.connect()
+                query = scores.select().where(scores.c.user_id == message.author.id)
+                result = conn.execute(query)
+                row = result.fetchone()
+                if row is not None:
+                    score = row.score
+                    update = scores.update().where(scores.c.user_id == message.author.id).values(score=score + 1)
+                    conn.execute(update)
+                else:
+                    insert = scores.insert().values(user_id=message.author.id, score=1)
+                    conn.execute(insert)
 
 def setup(bot):
     bot.add_cog(Catcher(bot))
